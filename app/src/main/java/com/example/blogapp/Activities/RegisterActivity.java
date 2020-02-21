a30b368051a4329d7a265c0e0571cb6d1886c14f
BiruCode <yeisonplox@gmail.com>
2020-02-21 17:26:44 -0500

    Seleccionar imagen de la galeria, manejo de permisos

--- app/src/main/java/com/example/blogapp/Activities/RegisterActivity.java
+++ app/src/main/java/com/example/blogapp/Activities/RegisterActivity.java
@@ -1,16 +1,85 @@
 package com.example.blogapp.Activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 import com.example.blogapp.R;
+import java.net.URI;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    ImageView ImgUserPhoto;
+    static int PReqCode=1;
+    static int REQUESCODE=1;
+    Uri pickedImgUri;//identificador de recursos uniforme guardar imagen de el usuario
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        ImgUserPhoto = (ImageView)findViewById(R.id.regUserPhoto);
+
+        ImgUserPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(Build.VERSION.SDK_INT >= 22){
+
+                    checkAndRequestForPermission();
+
+                }else{
+                    openGallery();
+                }
+            }
+        });
     }
+
+    private void openGallery() {
+        //TODO: Abrir la galeria y esperar a que el usuario seleccione una imagen
+
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent,REQUESCODE);
+    }
+
+    private void checkAndRequestForPermission() {
+
+        if(ContextCompat.checkSelfPermission(RegisterActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+        != PackageManager.PERMISSION_GRANTED){//sino se tiene el permiso de leer el almacenamiento externo
+
+            if(ActivityCompat.shouldShowRequestPermissionRationale(RegisterActivity.this,Manifest.permission.READ_EXTERNAL_STORAGE)){//si no se acepta los permisos
+                Toast.makeText(this, "Please accept for required permission", Toast.LENGTH_SHORT).show();
+            }else{//mostrar el recuadro para solicitar los permisos y guardar que si se dio el permiso
+                ActivityCompat.requestPermissions(RegisterActivity.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        PReqCode);
+            }
+        }else{//si ya se tiene el permiso de ingresar al almacenamiento externo
+            openGallery();//abrir galeria
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK && requestCode == REQUESCODE && data != null){
+            //el usuario ha elegido con exito una imagen de su galeria y necesitamos guardar su referencia en una variable
+            pickedImgUri = data.getData();//guardar la imagen seleccionada por el usuario
+            ImgUserPhoto.setImageURI(pickedImgUri);//poner la imagen seleccionada por el usuario en el imageview
+        }
+    }
+
 }
