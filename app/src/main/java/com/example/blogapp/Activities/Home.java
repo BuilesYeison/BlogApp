3eb19189d6f8d9fdec9e8011dde9634186f24a32
BiruCode <yeisonplox@gmail.com>
2020-03-19 16:20:04 -0500

    Creacion de popup para poder subir posts al aplicativo, cargar foto de perfil de usuario, campos y otras cosas del popUp

--- app/src/main/java/com/example/blogapp/Activities/Home.java
+++ app/src/main/java/com/example/blogapp/Activities/Home.java
@@ -1,14 +1,16 @@
 package com.example.blogapp.Activities;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import com.bumptech.glide.Glide;
 import com.example.blogapp.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
-import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 
 import androidx.annotation.NonNull;
@@ -29,13 +31,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class Home extends AppCompatActivity {
 
     FirebaseUser currentUser;//creamos el objeto que contiene la infromacion del usuario
     FirebaseAuth mAuth;//y la autentificacion firebase
+    Dialog popAddPost;//creamos este objeto para agregar el layout que creamos del popup para añadir publicaciones
+    ImageView popupUserImage, popupPostImage, popupAddBtn;//creamos los dos objetos correspondientes al foto de perfil que irá en el pop up, la imagen que cargará el usuario desde su galeria y para el boton de agregar
+    ProgressBar popupProgressBar;
+    EditText popupTittle, popupDescription;
+
     private AppBarConfiguration mAppBarConfiguration;
 
     @Override
@@ -45,16 +54,18 @@ public class Home extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        //initialize
+        //initialize user data obtain
         mAuth = FirebaseAuth.getInstance();//obtenemoe la autentificacion del usuario actual
         currentUser = mAuth.getCurrentUser();//y en el objeto que contendra la info del usuario asignamos la info del usuario que esta actualemnte en linea
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+        //initialize PopUp
+        iniPopup();//llamamos nuestro metodo que nos muestra en pantalla el popup para añadir publicaciones
+
+        FloatingActionButton fab = findViewById(R.id.fab);//este es el boton flotante que por defecto tiene un icono de mensajería, aquí agregaremos nuestro popup para crear contenido dentro de la app
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                popAddPost.show();//mostramos el popup
             }
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -83,6 +94,33 @@ public class Home extends AppCompatActivity {
         });
 
         updateNavHeader();//llamamos el metodo que actualiza la info en el nav header
+    }
+
+    private void iniPopup() {
+        popAddPost = new Dialog(this);//decimos el contexto donde se cargará el popup
+        popAddPost.setContentView(R.layout.popup_add_post);//le asignamos a la variable la vista de el popup que creamos
+        popAddPost.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));//obtenemos la ventana donde se abre nuestro popup y le damos un color de fondo transparente para que el popup no se vea tan solido sino por encima del home
+        popAddPost.getWindow().setLayout(Toolbar.LayoutParams.MATCH_PARENT,Toolbar.LayoutParams.WRAP_CONTENT);//escondemos la barra de tareas (la barra azul superior) porque allí se ubicará nuestro popup
+        popAddPost.getWindow().getAttributes().gravity = Gravity.TOP;//obtenemos la ventana actual donde se da la ejecucion del popup y le decimos que ubique nuestro popup en la parte de arriba de la ventana
+
+        popupUserImage = popAddPost.findViewById(R.id.popup_user_image);//hacemos la conexion de lso objeto que acabamos de crear con los obejtos de nuestro popUp
+        popupAddBtn = popAddPost.findViewById(R.id.popup_add);
+        popupDescription = popAddPost.findViewById(R.id.popup_description);
+        popupTittle = popAddPost.findViewById(R.id.popup_tittle);
+        popupPostImage = popAddPost.findViewById(R.id.popup_image);
+        popupProgressBar = popAddPost.findViewById(R.id.popup_progressBar);
+
+        //cargar foto de perfil del usuario en el popup
+        Glide.with(Home.this).load(currentUser.getPhotoUrl()).into(popupUserImage);
+
+        //añadir evento cuando da click en el boton para agregar publicacion
+        popupAddBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                popupProgressBar.setVisibility(View.VISIBLE);//cuando aprete en el boton agregar aparecera el progressbar
+                popupAddBtn.setVisibility(View.INVISIBLE);//y el btn lo ponemos invisible
+            }
+        });
     }
 
     private void cerrarSesion() {//metodo para cerrar sesion del usuario y para redirigirlo al activity inicio de sesion
